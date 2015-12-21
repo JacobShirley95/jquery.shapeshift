6e1d3a5a3fe0ab88944f242cebab6a10ed05783c
Jacob Shirley <auriferous95@gmail.com>
2015-12-21 13:03:23 +0000

    Removed minifier.

--- minify.js
+++
@@ -1,9 +0,0 @@
-var compressor = require('node-minify');
-
-new compressor.minify({
-  type: 'yui-js',
-  fileIn: 'core/jquery.shapeshift.js',
-  fileOut: 'core/jquery.shapeshift.min.js',
-  callback: function(err, min){
-  }
-});
